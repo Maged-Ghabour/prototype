f4deb3a68353c4278f8b927241164d8d025b8680
MagedGhabour <92618945+Maged-Ghabour@users.noreply.github.com>
2026-06-08 23:44:17 +0300

    add new feature

--- resources/views/filament/pages/settings-page.blade.php
+++ resources/views/filament/pages/settings-page.blade.php
@@ -9,11 +9,11 @@
         <div class="mb-4 flex justify-end">
             <div class="p-3 bg-white dark:bg-gray-800 rounded-xl shadow border border-gray-200 dark:border-gray-700 inline-block">
                 <p class="text-xs text-gray-500 mb-2 text-right">اللوجو الحالي:</p>
-                <img
-                    src="{{ Storage::disk('public')->url($logoPath) }}"
-                    alt="لوجو التطبيق"
-                    class="h-16 object-contain"
-                >
+                    <img
+                        src="{{ Storage::disk('public')->url($logoPath) }}"
+                        alt="لوجو التطبيق"
+                        class="h-16 max-w-[150px] object-contain"
+                    >
             </div>
         </div>
     @endif
