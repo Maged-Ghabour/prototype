9e46d4c9b647e24e57f2315f5d745be9115b13c7
unknown <maged.ghabour2016@gmail.com>
2026-06-30 16:35:31 +0300

    size

--- resources/views/prototype/raw.blade.php
+++ resources/views/prototype/raw.blade.php
@@ -1,3 +1,28 @@
+@php
+    $html = trim($prototype->html_code);
+    $isFullHtml = str_contains(strtolower($html), '<html');
+
+    if ($isFullHtml) {
+        $css = $prototype->css_code ? "<style>{$prototype->css_code}</style>" : '';
+        $js = $prototype->js_code ? "<script>{$prototype->js_code}</script>" : '';
+
+        if ($css && str_contains(strtolower($html), '</head>')) {
+            $html = preg_replace('/<\/head>/i', $css . '</head>', $html, 1);
+        } else {
+            $html = $css . $html;
+        }
+        
+        if ($js && str_contains(strtolower($html), '</body>')) {
+            $html = preg_replace('/<\/body>/i', $js . '</body>', $html, 1);
+        } else {
+            $html = $html . $js;
+        }
+    }
+@endphp
+
+@if ($isFullHtml)
+{!! $html !!}
+@else
 <!DOCTYPE html>
 <html lang="ar">
 <head>
@@ -28,3 +53,4 @@
 
 </body>
 </html>
+@endif
